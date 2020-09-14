bbd134f2378234adc4e7e821d9c712d3ccb3f61f
Jose Acitores <acitores.jm@gmail.com>
2020-09-14 14:35:35 +0200

    Changed bow to be function based

--- BagOfWords.py
+++ BagOfWords.py
@@ -1,28 +1,31 @@
 import nltk
-import numpy as np
-import random
-import string
 import heapq
-from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import MultinomialNB
 
-wordfreq = {}
-for content in contents:
-    tokens = nltk.word_tokenize(content[0])
-    for token in tokens:
-        if token not in wordfreq.keys():
-            wordfreq[token] = 1
-        else:
-            wordfreq[token] += 1
-most_freq = heapq.nlargest(200, wordfreq, key=wordfreq.get)
 
-content_vectors = []
-for content in contents:
-    content_tokens = nltk.word_tokenize(content[0])
-    sent_vec = []
-    for token in most_freq:
-        if token in content_tokens:
-            sent_vec.append(1)
-        else:
-            sent_vec.append(0)
-    content_vectors.append(sent_vec)
+def get_word_freq(contents):
+    wordfreq = {}
+    for content in contents:
+        tokens = nltk.word_tokenize(content[0])
+        for token in tokens:
+            if token not in wordfreq.keys():
+                wordfreq[token] = 1
+            else:
+                wordfreq[token] += 1
+    most_freq = heapq.nlargest(200, wordfreq, key=wordfreq.get)
+
+    return most_freq
+
+
+def get_bow(contents, most_freq):
+    content_vectors = []
+    for content in contents:
+        content_tokens = nltk.word_tokenize(content[0])
+        sent_vec = []
+        for token in most_freq:
+            if token in content_tokens:
+                sent_vec.append(1)
+            else:
+                sent_vec.append(0)
+        content_vectors.append(sent_vec)
+
+    return content_vectors
