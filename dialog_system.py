6b8696a9676067f73fa7a63bdc47a2c931f4b8a1
Jose Acitores <acitores.jm@gmail.com>
2020-09-28 22:42:53 +0200

    Added comments and unified sources

--- dialog_system.py
+++ dialog_system.py
@@ -1,93 +1,20 @@
 import pandas as pd
-from BagOfWords import get_bow_unpacked
-
-
-class DialogSystem:
-
-    def __init__(self, restaurant_data, ml_model, frequent_words):
-        self.state = Welcome({})
-        self.data = pd.read_csv(restaurant_data)
-        self.ml_model = ml_model
-        self.entities = {}
-        self.entities_options = self.__get_unique_entities(self.data)
-        self.frequent_words = frequent_words
-        self.preferences = {}
-
-    def __get_restaurants(self, restaurantname=None,pricerange=None,area=None,food=None,phone=None,addr=None,postcode=None):
-        dataframe = self.data
-        if restaurantname is not None:
-            dataframe = dataframe[dataframe['restaurantname'] == restaurantname]
-        if pricerange is not None:
-            dataframe = dataframe[dataframe['pricerange'] == pricerange]
-        if area is not None:
-            dataframe = dataframe[dataframe['area'] == area]
-        if food is not None:
-            dataframe = dataframe[dataframe['food'] == food]
-        if phone is not None:
-            dataframe = dataframe[dataframe['phone'] == phone]
-        if addr is not None:
-            dataframe = dataframe[dataframe['addr'] == addr]
-        if postcode is not None:
-            dataframe = dataframe[dataframe['postcode'] == postcode]
-        return dataframe
-
-    def __get_unique_entities(self, data):
-        field_list = ['pricerange', 'area', 'food', 'postcode']
-        field_dict = {}
-        for field in field_list:
-            field_dict[field] = data[field].unique()
-        return field_dict
-
-    def __get_entities(self, sentence):
-        entities = {}
-        for word in sentence:
-            for key, value in self.entities_options.items():
-                if word in value:
-                    entities[key] = word
-        return entities
-
-    def __transition(self, new_state):
-        if new_state.tag == 'welcome':
-            self.preferences = {}
-        self.state = new_state
-
-    def __get_intent(self, sentence):
-        input_vector = get_bow_unpacked(sentence, self.frequent_words)
-        return self.ml_model.predict(input_vector)
-
-    def __update_preferences(self, entities):
-        print(entities)
-        for key, value in entities.items():
-            self.preferences[key] = value
-
-
-    def process_sentence(self, sentence):
-        sentence = sentence.split(' ')
-        intent = self.__get_intent(sentence)
-        entities = self.__get_entities(sentence)
-        self.__update_preferences(entities)
-        df = None
-        if self.preferences != {}:
-            #self.entities = self.state.assure_entities(entities)
-            df = self.__get_restaurants(pricerange=self.preferences.get('pricerange', None),
-                                        area=self.preferences.get('area', None),
-                                        food=self.preferences.get('food', None),
-                                        postcode=self.preferences.get('postcode', None))
-        self.__transition(self.state.get_next_state(intent, self.preferences, df))
-
-        return intent
-
-    def get_message(self):
-        self.state.print_message()
+from utils import get_bow_unpacked
 
 
 class State:
+    """
+    This class is an abstract class that represents the state in which the dialog is in the moment, for that purpose,
+    several classes inherit from State to create  the different options
+    """
     states_dict: {}
     message: {}
     preferences: {}
     tag: str
 
     def get_next_state(self, intent, preferences_user, df):
+        """This function is a generic function for all the states to change state, it depends on the features of every
+        class inheriting from State."""
         if df is None:
             df = []
         next_state = None
@@ -116,6 +43,8 @@ class State:
             return Repeat(preferences_user)
 
     def assure_entities(self, entities):
+        """This class is made to assure the preferences of the user, currently not used but will be possible to
+        activate it in the final version of the project"""
         assured_entities = {}
         print(entities)
         for category, entity in entities.items():
@@ -140,10 +69,12 @@ class State:
         return assured_entities
 
     def print_message(self):
+        """This function prints the message of every class"""
         print(self.message)
 
 
 class Welcome(State):
+    """State to welcome and initiate the conversation, most of the time it will advance to preferences"""
     def __init__(self, preferences_user):
         self.states_dict = {'Preferences': ['inform', 'hello'], 'repeat': ['null']}
         self.message = 'Hello, how can I help you?'
@@ -152,7 +83,9 @@ class Welcome(State):
 
 
 class Preferences(State):
-
+    """This state is the longest and it is the one in which the user can set their preferences when it comes to finding
+    a restaurant. It has various messages in case some preferences are missing, in order to create a more accurate
+    search"""
     def __init__(self, preferences_user):
         self.states_dict = {'Preferences': ['inform'],'repeat': ['null']}
         self.message = {'first' : 'Can you tell me what you are looking for?',
@@ -163,6 +96,7 @@ class Preferences(State):
         self.tag = 'preferences'
 
     def print_message(self):
+        """Preferences has a custom print message function in order to handle missing information in the preferences"""
         if self.preferences == {}:
             print(self.message['first'])
         else:
@@ -177,6 +111,8 @@ class Preferences(State):
 
 
 class ChangePreferences(State):
+    """This State Class is used when all the preferences have been used but the user wants to change some of them in
+    order to find a different restaurant from the offered ones"""
     def __init__(self, preferences_user):
         self.states_dict = {'Preferences': ['inform', 'affirm'], 'Finish': ['negate'], 'repeat': ['null']}
         self.message = 'Do you want to change any preference? If yes tell me, or else say no'
@@ -185,6 +121,8 @@ class ChangePreferences(State):
 
 
 class Repeat(State):
+    """This State Class is used when the sentence from the user was not understood and repeats the same State as before
+    """
     def __init__(self, preferences_user):
         self.states_dict = {'Preferences': ['inform'], 'repeat': []}
         self.message = 'Can you repeat?'
@@ -193,10 +131,107 @@ class Repeat(State):
 
 
 class Finish(State):
+    """This State Class is used when the process of looking or a restaurant is finished, therefore we offer the user the
+     possibility to look for a new restaurant or exit the program"""
     def __init__(self, preferences_user):
         self.states_dict = {'Welcome': ['affirm'],  'End': ['negate'], 'repeat': ['null']}
         self.message = 'Thank you, do you need anything else?'
         self.preferences = preferences_user
         self.tag = 'finish'
+
+
+class DialogSystem:
+    """
+    This class models a complete dialog system for the restaurant chatbot, it has states to which it transitions to
+    simulate the actual transitions of a normal conversation to find a restaurant. Some functions have been implemented
+    in order to access the data base and retrive important information from the input text
+    """
+
+    def __init__(self, restaurant_data: str, ml_model, frequent_words):
+        self.state = Welcome({})
+        self.data = pd.read_csv(restaurant_data)
+        self.ml_model = ml_model
+        self.entities = {}
+        self.__get_unique_entities()
+        self.frequent_words = frequent_words
+        self.preferences = {}
+
+    def __get_restaurants(self, restaurantname: str = None, pricerange: str = None, area: str = None,
+                          food: str = None, phone: str = None, addr: str = None, postcode: str = None):
+        """This function gets the restaurants available in the database with the user preferences"""
+        dataframe = self.data
+        if restaurantname is not None:
+            dataframe = dataframe[dataframe['restaurantname'] == restaurantname]
+        if pricerange is not None:
+            dataframe = dataframe[dataframe['pricerange'] == pricerange]
+        if area is not None:
+            dataframe = dataframe[dataframe['area'] == area]
+        if food is not None:
+            dataframe = dataframe[dataframe['food'] == food]
+        if phone is not None:
+            dataframe = dataframe[dataframe['phone'] == phone]
+        if addr is not None:
+            dataframe = dataframe[dataframe['addr'] == addr]
+        if postcode is not None:
+            dataframe = dataframe[dataframe['postcode'] == postcode]
+        return dataframe
+
+    def __get_unique_entities(self):
+        """This function gets the a dictionary of the unique options in the dataset for every feature"""
+        field_list = ['pricerange', 'area', 'food', 'postcode']
+        field_dict = {}
+        for field in field_list:
+            field_dict[field] = self.data[field].unique()
+        self.entities_options = field_dict
+
+    def __get_entities(self, sentence: str):
+        """This function gets all the entities from a given sentence looking in the unique dictionary"""
+        entities = {}
+        for word in sentence:
+            for key, value in self.entities_options.items():
+                if word in value:
+                    entities[key] = word
+        return entities
+
+    def __transition(self, new_state: State):
+        """This function makes a transition between states of the flow"""
+        if new_state.tag == 'welcome':
+            self.preferences = {}
+        self.state = new_state
+
+    def __get_intent(self, sentence: str):
+        """This function gets the intent of a given sentence"""
+        input_vector = get_bow_unpacked(sentence, self.frequent_words)
+        return self.ml_model.predict(input_vector)
+
+    def __update_preferences(self, entities: {}):
+        """This function updates the preferences of the user with new given entities"""
+        print(entities)
+        for key, value in entities.items():
+            self.preferences[key] = value
+
+    def process_sentence(self, sentence: str):
+        """
+        This function process the sentence running multiple of the previous functions in order to extract the
+        entities, the intent and  update the user preferences. It creates a dataframe with the available restaurants and
+        assures that the state transition is done."""
+        sentence = sentence.split(' ')
+        intent = self.__get_intent(sentence)
+        entities = self.__get_entities(sentence)
+        self.__update_preferences(entities)
+        df = None
+        if self.preferences != {}:
+            #self.entities = self.state.assure_entities(entities)
+            df = self.__get_restaurants(pricerange=self.preferences.get('pricerange', None),
+                                        area=self.preferences.get('area', None),
+                                        food=self.preferences.get('food', None),
+                                        postcode=self.preferences.get('postcode', None))
+        self.__transition(self.state.get_next_state(intent, self.preferences, df))
+
+        return intent
+
+    def get_message(self):
+        """This function calls the function of state to print a message of a specific state."""
+        self.state.print_message()
 
 
--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 from sklearn import model_selection
+import nltk
+import heapq
 import numpy as np
 
 
@@ -17,3 +19,69 @@ def split_data(data):
     print(y_test.shape)
 
     return X_train, X_test, y_train, y_test
+
+
+def reasoning(list_in):
+    list_update = list_in
+    list_copy = []
+    while list_update != list_copy:
+        list_copy = list_update.copy()
+        if 'spanish' in list_in and 'long time' not in list_in:
+            list_update.append('long time')
+        if 'cheap' in list_in and 'not romantic' not in list_in:
+           list_update.append('not romantic')
+        if 'expensive' in list_in and 'romantic' not in list_in:
+            list_update.append('romantic')
+        if 'expensive' in list_in and 'good food' not in list_in:
+            list_update.append('good food')
+        if 'centre' in list_in and 'busy' not in list_in:
+            list_update.append('busy')
+        if 'busy' in list_in and 'late' not in list_in:
+            list_update.append('late')
+        if 'long time' in list_in and 'late' not in list_in:
+            list_update.append('late')
+        if 'cheap' in list_in and 'good food' in list_in and 'busy' not in list_in:
+            list_update.append('busy')
+        if 'gastropub' in list_in and 'not children' not in list_in:
+            list_update.append('not children')
+    return list_update
+
+
+def get_word_freq(contents):
+    wordfreq = {}
+    for content in contents:
+        tokens = nltk.word_tokenize(content[0])
+        for token in tokens:
+            if token not in wordfreq.keys():
+                wordfreq[token] = 1
+            else:
+                wordfreq[token] += 1
+    most_freq = heapq.nlargest(200, wordfreq, key=wordfreq.get)
+
+    return most_freq
+
+
+def get_bow(contents, most_freq):
+    content_vectors = []
+    for content in contents:
+        content_tokens = nltk.word_tokenize(content[0])
+        sent_vec = []
+        for token in most_freq:
+            if token in content_tokens:
+                sent_vec.append(1)
+            else:
+                sent_vec.append(0)
+        content_vectors.append(sent_vec)
+
+    return content_vectors
+
+
+def get_bow_unpacked(contents, most_freq):
+    content_vectors = np.zeros(len(most_freq))
+    for content in contents:
+        content_tokens = nltk.word_tokenize(content)
+        for i in range(len(most_freq)):
+            if most_freq[i] in content_tokens:
+                content_vectors[i] = 1
+
+    return [content_vectors]
